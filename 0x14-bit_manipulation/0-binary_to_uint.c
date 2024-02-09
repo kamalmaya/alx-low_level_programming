73233761b888a0689f89e7c0877d05a99ebebc83
kamalmaya <kamalmaya02@gmail.com>
2024-02-09 00:01:18 -0800

    hello

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int i;
-
-    if (b == NULL)
-        return (0);
-
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
-
-        result = (result << 1) | (b[i] - '0');
-    }
-
-    return (result);
+unsigned int result = 0;
+int i;
+if (b == NULL)
+return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+result = (result << 1) | (b[i] - '0');
+}
+return (result);
 }
