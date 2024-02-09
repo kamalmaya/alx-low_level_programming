cedbc28de22a17bf87ec3ebce7f4dd731163e3f9
kamalmaya <kamalmaya02@gmail.com>
2024-02-09 07:16:17 -0800

    hy

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include"main.h"
+
+int get_endianness(void)
+{
+unsigned int num = 1;
+char *ptr = (char *)&num;
+if (*ptr)
+return (1); /** little endian */
+else
+return (0); /** big endian */
+}
